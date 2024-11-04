07a4c49550ba98814330b04df13496806c7d61fb
OwaisZakir <owaiszakir88@gmial.com>
2024-11-04 11:22:29 +0500

    add font and gsap

--- gsap/animate.js
+++ gsap/animate.js
@@ -1,22 +1,47 @@
+let animateTime = 0.8;
+let webHeader = document.querySelector("header");
+gsap.from(webHeader, {
+  top: "-100%",
+  duration: animateTime,
+  ease: "slow(0.7,0.7,false)",
+});
+
 let navTl = gsap.timeline();
 let openMenuBtn = document.querySelector("#openMenu");
+let closeMenuBtn = document.querySelector("#closeBtn");
+
+navTl.to("nav", {
+  right: 0,
+  duration: 0.8,
+  ease: "sine.in",
+});
+navTl.from(closeMenuBtn, {
+  y: -20,
+  opacity: 0,
+  duration: .7,
+});
+navTl.from("#navLink", {
+  x: 200,
+  opacity: 0,
+  duration: 0.6,
+  stagger: 0.25,
+});
+
+navTl.pause();
 
 openMenuBtn.addEventListener("click", () => {
-  navTl.to("nav", {
-    right: 0,
+  gsap.from(openMenuBtn, {
+    scale: 0.9,
     duration: 0.5,
-    ease: "bounce.in",
   });
-  navTl.from("#navLink", {
-    x: 200,
-    opacity: 0,
-    duration: .6,
-    stagger:1
-  });
+  navTl.play();
 });
-// navTl.from("#navLink",{
-//     x:200,
-//     opacity:0,
-//     daley:1,
-//     duration:2
-// })
+
+closeMenuBtn.addEventListener("click", () => {
+  gsap.from(openMenuBtn, {
+    scale: 1,
+    rotate: 360,
+    duration: 0.5,
+  });
+  navTl.reverse();
+});
--- gsap/dist/animate.dev.js
+++ gsap/dist/animate.dev.js
@@ -1,22 +1,44 @@
 "use strict";
 
+var animateTime = 0.8;
+var webHeader = document.querySelector("header");
+gsap.from(webHeader, {
+  top: "-100%",
+  duration: animateTime,
+  ease: "slow(0.7,0.7,false)"
+});
 var navTl = gsap.timeline();
 var openMenuBtn = document.querySelector("#openMenu");
+var closeMenuBtn = document.querySelector("#closeBtn");
+navTl.to("nav", {
+  right: 0,
+  duration: 0.8,
+  ease: "sine.in"
+});
+navTl.from(closeMenuBtn, {
+  y: -20,
+  opacity: 0,
+  duration: .7
+});
+navTl.from("#navLink", {
+  x: 200,
+  opacity: 0,
+  duration: 0.6,
+  stagger: 0.25
+});
+navTl.pause();
 openMenuBtn.addEventListener("click", function () {
-  navTl.to("nav", {
-    right: 0,
-    duration: 0.5,
-    ease: "bounce.in"
+  gsap.from(openMenuBtn, {
+    scale: 0.9,
+    duration: 0.5
   });
-  navTl.from("#navLink", {
-    x: 200,
-    opacity: 0,
-    duration: .6,
-    stagger: 1
+  navTl.play();
+});
+closeMenuBtn.addEventListener("click", function () {
+  gsap.from(openMenuBtn, {
+    scale: 1,
+    rotate: 360,
+    duration: 0.5
   });
-}); // navTl.from("#navLink",{
-//     x:200,
-//     opacity:0,
-//     daley:1,
-//     duration:2
-// })
+  navTl.reverse();
+});
